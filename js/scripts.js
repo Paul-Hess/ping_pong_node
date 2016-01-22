44e94d1ff701c53b3078edbd1b8d8b999a112873
Paul-Hess <pauljacksonhess@gmail.com>
2016-01-22 09:28:31 -0800

    refactor for return of range of numbers plus ping and/or pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var pingArray = [];
+
 // handles input number, verifies is a number and calls for return of ouput.
 var pingPong = function(num) {
 	if (isNumber(num)) {
@@ -9,15 +11,18 @@ var pingPong = function(num) {
 
 // returns ping and/or pong for 3 5 15 factorials.
 var numberReturn = function(num) {
-	if (num%15 === 0) {
-		return "pingpong";
-	} else if (num%5 === 0) {
-		return "pong";
-	} else if (num%3 === 0) {
-		return "ping";
-	} else {
-		return num;
-	} 
+	for (var i = 1; i <= num; i++){
+		if (i%15 === 0) {
+			pingArray.push("pingpong");
+		} else if (i%5 === 0) {
+			pingArray.push("pong");
+		} else if (i%3 === 0) {
+			pingArray.push("ping");
+		} else {
+			pingArray.push(i);
+		} 
+	}
+	return pingArray;
 }
 
 // aids in filtering unwanted input.
--- spec/specs.js
+++ spec/specs.js
@@ -1,64 +1,67 @@
 describe("pingPong", function() {
 	it('should reject non number inputs', function() {
-		expect(pingPong('foo')).to.equal("please enter a numeral");
+		expect(pingPong('foo')).to.eql("please enter a numeral");
 	});
 
 	it('should allow number inputs', function() {
-		expect(pingPong(5002)).to.equal(5002);
+		expect(pingPong(3)).to.eql([1, 2, 'ping']);
 	});
 
 	it('should return number when number is input', function() {
-		expect(pingPong(7)).to.equal(7);
+		expect(pingPong(7)).to.eql([7]);
 	});
 
 	it('should return pingpong for input of factor of 15', function() {
-		expect(pingPong(45)).to.equal("pingpong");
+		expect(pingPong(45)).to.eql("pingpong");
 	});
 
 	it('should not return pingpong for input of non-factor of 15', function() {
-		expect(pingPong(42)).to.not.equal("pingpong");
+		expect(pingPong(42)).to.not.eql("pingpong");
 	});
 
 	it('should return pong for factors of 5', function() {
-		expect(pingPong(65)).to.equal("pong");
+		expect(pingPong(65)).to.eql("pong");
 	});
 
 	it('should not return pong for non-factors of 5', function() {
-			expect(pingPong(30)).to.not.equal('pong');
+			expect(pingPong(30)).to.not.eql('pong');
 		});
 
 	it('should return ping for factors of 3', function() {
-		expect(pingPong(21)).to.equal('ping');
+		expect(pingPong(21)).to.eql('ping');
 	});
 
 	it('should not return ping for non-factors of 3', function() {
-		expect(pingPong(23)).to.not.equal('ping');
+		expect(pingPong(23)).to.not.eql('ping');
 	});
 
+	it('should return all numbers in the range with the ping and pong exceptions', function() {
+		expect(pingPong(12)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping'])
+	});
 });
 
 describe('numberReturn', function() {
 	it('should return pingpong for input of factor of 15', function() {
-		expect(numberReturn(45*7)).to.equal("pingpong");
+		expect(numberReturn(45*7)).to.eql("pingpong");
 	});
 
 	it('should not return pingpong for input of non-factor of 15', function() {
-		expect(numberReturn(43*7)).to.not.equal("pingpong");
+		expect(numberReturn(43*7)).to.not.eql("pingpong");
 	});
 
 	it('should return pong for factors of 5', function() {
-		expect(numberReturn(6860)).to.equal('pong');
+		expect(numberReturn(6860)).to.eql('pong');
 	});
 
 	it('should not return pong for non-factors of 5', function() {
-			expect(numberReturn(34)).to.not.equal('pong');
+			expect(numberReturn(34)).to.not.eql('pong');
 		});
 
 	it('should return ping for factors of 3', function() {
-		expect(numberReturn(432)).to.equal('ping');
+		expect(numberReturn(432)).to.eql('ping');
 	});
 
 	it('should not return ping for non-factors of 3', function() {
-		expect(numberReturn(35)).to.not.equal('ping');
+		expect(numberReturn(35)).to.not.eql('ping');
 	});
 });
