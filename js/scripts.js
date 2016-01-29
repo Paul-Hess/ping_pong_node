34821f0008f37e7059ea22c5a7d66ec9c122c0a7
Paul-Hess <pauljacksonhess@gmail.com>
2016-01-28 19:02:28 -0800

    refactor tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,6 @@
-// holds returned numbers and strings for passing to UI.
-var pingArray = [];
-
-// handles input number, verifies is a number and calls for return of ouput.
-var pingPong = function(num) {
-	if (num > 0 && num < 501) {
-		return numberReturn(num);
-	} else {
-		alert("please enter a positive numeral"); 	
-	}
-}
-
 // returns ping and/or pong for 3 5 15 factorials.
-var numberReturn = function(num) {
-	pingArray = [];	
+var pingPong = function(num) {
+	var pingArray = [];	
 	for (var i = 1; i <= num; i++){
 		if (i%15 === 0) {
 			pingArray.push("pingpong");
@@ -28,30 +16,32 @@ var numberReturn = function(num) {
 }
 
 $(function() {
-	//assigns index for iterations through loop of returns.
-	var count = 0;
-
-	//helper function to forEach to add list items to ul.
-	function addListItems() {
-		$('ul.output').append(document.createElement('li'));
-		$('li').last().append(pingArray[count]);
-		count++;	
-	}
-
-	$('form#number-input').on('submit', function(event) {
+	$('html, body').scrollTop(0);
+	
+	$('form#number-input').submit(function(event) {
+		event.preventDefault();
+		$('li.listItem').remove();	
 		var numberInput = $('input#toPing').val();
-		pingPong(numberInput);
-		pingArray.forEach(addListItems)
-		$('html, body').animate({
-                    scrollTop: $("li").last().offset().top
-                		}, 3000);
-		$('div.hide-button').show();
-		event.preventDefault();	
+		if (parseFloat(numberInput) > 0) {
+			pingPong(numberInput).forEach(function(index) {
+				$('ul.output').append('<li class="listItem"></li>');
+				$('li').last().text(index);
+			});
+			$('div.hide-button').show();
+			if ($('li.listItem').length > 25) {
+				$('html, body').animate({
+												scrollTop: $(".reset").last().offset().top
+												}, 0);
+			};	
+		} else {
+			$('ul.output').append('<li class="listItem"></li>');
+			$('li').last().text("please enter a positive numeral");
+		}
 	});
 
 	$('.reset').click(function() {
 		location.href = "index.html"
-		$(this).scrollTop(0);
+		$(document).scrollTop(0);
 	});
 
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,81 +1,27 @@
 describe("pingPong", function() {
 
-	alert = function() {};
-	
-	it('should reject non number inputs', function() {
-		expect(pingPong(-1)).to.equal(undefined);
+	it('should return a number ', function() {
+		expect(pingPong(2)).to.eql([1, 2]);
 	});
 
-	it('should not return a number array for input 0', function() {
-		expect(pingPong(0)).to.equal(undefined);
-	});
-
-	it('should not handle oversized numbers for the browser to handle jQuery animation', function() {
-		expect(pingPong(1001)).to.equal(undefined);
-	});
-
-	it('should allow number inputs', function() {
+	it('should return ping when 3 is input', function() {
 		expect(pingPong(3)).to.eql([1, 2, 'ping']);
 	});
 
-	it('should return array when number is input', function() {
-		expect(pingPong(7)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7]);
-	});
-
-	it('should return pingpong for input of factor of 15', function() {
-		expect(pingPong(15)).to.include('pingpong');
-	});
-
 	it('should return pingpong for input of factor of 5', function() {
-		expect(pingPong(5)).to.include('pong');
+		expect(pingPong(5)[4]).to.equal('pong');
 	});
 
 	it('should return pingpong for input of factor of 15', function() {
-		expect(pingPong(3)).to.include('ping');
+		expect(pingPong(15)[14]).to.equal('pingpong');
 	});
 
-	it('should return proper array 15 is input', function() {
-		expect(pingPong(15)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong']);
-	});
-
-	it('should pass number to numberReturn and get array back', function() {
-		expect(pingPong(3)).to.satisfy(function() {return []});
-	});
-
-	it('should not return pingpong when 15 is not satisfied', function() {
-		expect(pingPong(14)).to.not.include('pingpong');
-	});
-
-	it('should not return pong when 5 is not satisfied', function() {
-		expect(pingPong(4)).to.not.include('pong');
-	});
-
-	it('should not return ping when 3 is not satisfied', function() {
-		expect(pingPong(2)).to.not.include('ping');
+	it('should return proper string for more than just the first factor', function() {
+		expect(pingPong(30)[3]).to.equal(4);
+		expect(pingPong(30)[5]).to.equal('ping');
+		expect(pingPong(30)[9]).to.equal('pong');
+		expect(pingPong(30)[29]).to.equal('pingpong');
 	});
 
 });
 
-describe('numberReturn', function() {
-	
-	it('should return array when number is input', function() {
-		expect(pingPong(9)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping']);
-	});
-
-	it('should return pingpong for input of factor of 15', function() {
-		expect(pingPong(15)).to.include('pingpong');
-	});
-
-	it('should return pingpong for input of factor of 3', function() {
-		expect(pingPong(3)).to.include('ping');
-	});
-
-	it('should return pingpong for input of factor of 5', function() {
-		expect(pingPong(5)).to.include('pong');
-	});
-
-	it('should return proper array when all  ping and pong factors are considered as input', function() {
-		expect(pingPong(30)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong', 16, 17, 'ping', 19, 'pong', 'ping', 22, 23, 'ping', 'pong', 26, 'ping', 28, 29, 'pingpong']);
-	});
-
-});
